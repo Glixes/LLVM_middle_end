3ae43e72aca97d978efb718c05b5d843b25281ef
Raffaele Tranfaglia <raffaele.tranfaglia@gmail.com>
2024-03-26 15:14:22 +0100

    add alternative version of LocalOpts.cpp

--- llvm/lib/Transforms/Utils/LocalOpts.cpp
+++ llvm/lib/Transforms/Utils/LocalOpts.cpp
@@ -39,8 +39,10 @@ bool runOnBasicBlock(BasicBlock &B)
       // create shift constant
       ConstantInt *shift = ConstantInt::get(C2->getType(), C2->getValue().logBase2());
       // create a new instruction shl instruction
-      NewShl = BinaryOperator::Create(BinaryOperator::Shl, fact2, shift);
+      NewShl = BinaryOperator::Create(BinaryOperator::Shl, fact1, shift);
     }
+    else
+      continue;
     // isert the new shift instruction after the considered mul instruction
     NewShl->insertAfter(&inst);
     // replace every the mul instruction use with a shift instruction use
@@ -49,6 +51,39 @@ bool runOnBasicBlock(BasicBlock &B)
 
   return true;
 }
+
+/*
+bool runOnBasicBlock(BasicBlock &B) 
+{
+  for (auto i = B.begin(); i != B.end(); i++)
+  {
+    Instruction &inst = *i;
+    if (inst.getOpcode() != BinaryOperator::Mul)
+      continue;
+
+    ConstantInt *C = dyn_cast<ConstantInt>(i->getOperand(1));
+    if (!C) 
+      continue;
+
+    const APInt value = C->getValue();
+    if (!value.isPowerOf2())
+      continue;
+
+    int factor = value.logBase2();
+
+    LLVMContext &context = i->getParent()->getContext();
+    Value *shift_val = ConstantInt::get(llvm::Type::getInt32Ty(context), factor);
+
+    Instruction *NewInst = BinaryOperator::Create(BinaryOperator::Shl, i->getOperand(0), shift_val);
+
+    NewInst->insertAfter(&inst);
+    inst.replaceAllUsesWith(NewInst);
+  }
+
+
+  return true;
+  }
+*/
 
 bool runOnFunction(Function &F) {
   bool Transformed = false;
