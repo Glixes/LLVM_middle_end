511056e01c1671e5fd3cdddeb2fb8aaa1d558d00
Matteo Venturi <test@testdomain.test>
2024-05-09 23:12:42 +0200

    fixed prints

--- llvm/lib/Transforms/Utils/LoopOpts.cpp
+++ llvm/lib/Transforms/Utils/LoopOpts.cpp
@@ -4,6 +4,7 @@ using namespace llvm;
 
 bool isAlreadyLoopInvariant(Instruction *inst, std::unordered_map<const Instruction*, bool> invariant_map)
 {
+    outs() << "Analyzing map: " << invariant_map[inst] << "\n";
     return invariant_map[inst];
     // return inst->getMetadata("invariant");
 }
@@ -11,10 +12,12 @@ bool isAlreadyLoopInvariant(Instruction *inst, std::unordered_map<const Instruct
 bool isValueInvariant(Value *v, Loop* L, std::unordered_map<const Instruction*, bool> invariant_map)
 {
     Instruction *v_inst = dyn_cast<Instruction>(v);
+    outs() << "Is value " << v_inst << " invariant?\n";
     if (!v_inst || isAlreadyLoopInvariant(v_inst, invariant_map) || !L->contains(v_inst))
         return true;
     
     Constant *c_inst = dyn_cast<Constant>(v);
+    outs() << "Is the value " << c_inst << " constant?\n";
     if (c_inst)
         return true;
     
@@ -27,6 +30,7 @@ bool isLoopInvariant(const Instruction *inst, Loop* L, std::unordered_map<const 
         return false;
     Value *val1 = inst->getOperand(0);
     Value *val2 = inst->getOperand(1);
+    outs() << "Analyzing operands: " << *val1 << ", " << *val2 << "\n";
     
     return isValueInvariant(val1, L, invariant_map) && isValueInvariant(val2, L, invariant_map);
 }
@@ -36,20 +40,20 @@ PreservedAnalyses LoopOpts::run (Loop &L, LoopAnalysisManager &LAM,
 {
     std::unordered_map<const Instruction*, bool> invariant_map;
     
-    outs() << "Pre-header: " << L.getLoopPreheader() << "\n";
-    outs() << "Header: " << L.getHeader() << "\n";
+    outs() << "Pre-header: " << *(L.getLoopPreheader()) << "\n";
+    outs() << "Header: " << *(L.getHeader()) << "\n";
     // outs() << L.getBlocks() << "\n";
     Loop *Loop = &L;
     for (Loop::block_iterator BI = L.block_begin(); BI != L.block_end(); ++BI)
     {
-        outs() << "Basic block: " << BI << "\n";
         BasicBlock const *BB = dyn_cast<BasicBlock const>(*BI);
+        outs() << "Basic block: " << *BB << "\n";
         for (BasicBlock::const_iterator i = BB->begin(); i != BB->end(); i++)
         {
             // const_cast translate a const object into a non constant one
             // in this case const Instruction into Instruction.
             const Instruction *inst = dyn_cast<Instruction>(i);
-            outs() << "Instruction: " << inst << "\n";
+            outs() << "Instruction: " << *inst << "\n";
             if (isLoopInvariant(inst, Loop, invariant_map))
             {
                 invariant_map[inst] = true;
@@ -58,7 +62,7 @@ PreservedAnalyses LoopOpts::run (Loop &L, LoopAnalysisManager &LAM,
                 MDNode *N = MDNode::get(C, MDString::get(C, ""));
                 inst->setMetadata("invariant", N);
                 */
-               outs() << "Loop invariant instruction detected: " << inst << "\n";
+               outs() << "Loop invariant instruction detected: " << *inst << "\n";
             }
         }
     }
