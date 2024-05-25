d30606b6c2ee2478207273d47bafad407c34ef37
Raffaele Tranfaglia <raffaele.tranfaglia@gmail.com>
2024-05-25 18:37:12 +0200

    add isAdjacent function

--- llvm/lib/Transforms/Utils/LoopFusion.cpp
+++ llvm/lib/Transforms/Utils/LoopFusion.cpp
@@ -1,6 +1,32 @@
 #include "llvm/Transforms/Utils/LoopFusion.h"
+#include "llvm/Transforms/Scalar/LoopPassManager.h"
+//#include <llvm/Analysis/LoopInfo.h>
 
 using namespace llvm;
 
+bool isAdjacent (Loop *l1, Loop *l2)
+{
+    // check for all the exiting blocks of l1
+    SmallVector<BasicBlock*, 4> exit_blocks;
+    l1->getUniqueNonLatchExitBlocks(exit_blocks);
+    for (BasicBlock *BB : exit_blocks)
+    {
+        if (BB != (l2->isGuarded() ? dyn_cast<BasicBlock>(l2->getLoopGuardBranch()) : l2->getLoopPreheader()))
+            return false;
+    }
+    return true;
+}
+
+
+
 PreservedAnalyses LoopFusion::run (Function &F,FunctionAnalysisManager &AM)
-{}
+{
+    LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+    DominatorTree &DT = AM.getResult<LoopStandardAnalysisResults>().getDomTree();
+    for (Loop *L : LI)
+    {
+        outs() << *L << "\n"; 
+    }
+
+    return PreservedAnalyses::all();
+}
