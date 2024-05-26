d20eb0c55df71e2f18a5aca4160d199e7de8e3fe
Raffaele Tranfaglia <raffaele.tranfaglia@gmail.com>
2024-05-26 18:11:08 +0200

    add getBaseAddress function

--- llvm/lib/Transforms/Utils/LoopFusion.cpp
+++ llvm/lib/Transforms/Utils/LoopFusion.cpp
@@ -20,11 +20,10 @@ bool isAdjacent (Loop *l1, Loop *l2)
 }
 
 
-
-bool haveSameNumberIterations (Loop *l1, Loop *l2, ScalarEvolution SE)
+bool haveSameNumberIterations (Loop *l1, Loop *l2, ScalarEvolution *SE)
 {
-    int c1 = SE.getSmallConstantMaxTripCount(l1);
-    int c2 = SE.getSmallConstantMaxTripCount(l2);
+    int c1 = SE->getSmallConstantMaxTripCount(l1);
+    int c2 = SE->getSmallConstantMaxTripCount(l2);
     return (c1 == c2);
 }
 
@@ -37,6 +36,39 @@ bool isFlowEquivalent (Loop *l1, Loop *l2, DominatorTree *DT, PostDominatorTree 
     return (DT->dominates(B1, B2) && PDT->dominates(B2, B1));
 }
 
+
+bool areDistanceIndependent (Loop *l1, Loop *l2)
+{
+    std::unordered_map<Value*, std::vector<std::vector<int, int>>> index_map;
+    for (auto BI = l1->block_begin(); BI != l2->block_end(); ++BI)
+    {
+        BasicBlock *BB = *BI;
+        for (auto i = BB->begin(); i != BB->end(); i++)
+        {
+            Instruction *inst = dyn_cast<Instruction>(i);
+            //index_map[dyn_cast<Value>(inst)].push_back()
+        }
+    }
+}
+
+
+Value *getBaseAddress (Instruction *inst)
+{
+    if (isa<AllocaInst>(inst) || isa<Argument>(inst))
+        return inst;
+    
+    Value *i;
+    for (Value::use_iterator iter = inst->use_begin(); iter != inst->use_end(); ++iter)
+    {
+        Use *use_of_inst = &(*iter);
+        Instruction *user_inst = dyn_cast<Instruction>(iter->getUser());
+
+        i = getBaseAddress(user_inst);
+        if (i)
+            return i;
+    }
+    return nullptr;
+}
 
 PreservedAnalyses LoopFusion::run (Function &F,FunctionAnalysisManager &AM)
 {
